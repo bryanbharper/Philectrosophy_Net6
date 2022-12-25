35122867da166f83e59a6ce70927d14a2c7d6b24
Bryan Harper <bryan@arborxr.com>
2022-12-25 15:35:13 -0600

    Add empty Client project

---
+++ Build/Main.fs
@@ -0,0 +1,81 @@
+﻿module Build.Main
+
+open Fake.Core
+open Fake.IO
+open Farmer
+open Farmer.Builders
+
+open Helpers
+
+initializeContext()
+
+let sharedPath = Path.getFullName "Shared"
+let serverPath = Path.getFullName "Server"
+let clientPath = Path.getFullName "Client"
+let deployPath = Path.getFullName "deploy"
+// let sharedTestsPath = Path.getFullName "tests/Shared"
+// let serverTestsPath = Path.getFullName "tests/Server"
+// let clientTestsPath = Path.getFullName "tests/Client"
+
+Target.create "Clean" (fun _ ->
+    Shell.cleanDir deployPath
+    run dotnet "fable clean --yes" clientPath // Delete *.fs.js files created by Fable
+)
+
+Target.create "InstallClient" (fun _ -> run npm "install" clientPath)
+
+Target.create "Bundle" (fun _ ->
+    [ "server", dotnet $"publish -c Release -o \"{deployPath}\"" serverPath
+      "client", dotnet "fable -o output -s --run npm run build" clientPath ]
+    |> runParallel
+)
+
+Target.create "Azure" (fun _ ->
+    let web = webApp {
+        name "Safe4._2._0"
+        operating_system OS.Windows
+        runtime_stack Runtime.DotNet60
+        zip_deploy "deploy"
+    }
+    let deployment = arm {
+        location Location.WestEurope
+        add_resource web
+    }
+
+    deployment
+    |> Deploy.execute "Safe4._2._0" Deploy.NoParameters
+    |> ignore
+)
+
+Target.create "Run" (fun _ ->
+    run dotnet "build" sharedPath
+    [ "server", dotnet "watch run" serverPath
+      "client", dotnet "fable watch -o output -s --run npm run start" clientPath ]
+    |> runParallel
+)
+
+// Target.create "RunTests" (fun _ ->
+//     run dotnet "build" sharedTestsPath
+//     [ "server", dotnet "watch run" serverTestsPath
+//       "client", dotnet "fable watch -o output -s --run npm run test:live" clientTestsPath ]
+//     |> runParallel
+// )
+
+open Fake.Core.TargetOperators
+
+let dependencies = [
+    "Clean"
+        ==> "InstallClient"
+        ==> "Bundle"
+        ==> "Azure"
+
+    "Clean"
+        ==> "InstallClient"
+        ==> "Run"
+
+    // "InstallClient"
+    //     ==> "RunTests"
+]
+
+[<EntryPoint>]
+let main args = runOrDefault args
